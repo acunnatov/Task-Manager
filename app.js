71be0612fa03033787a6bea47167a69064e31977
Abdulbosit <acunnatov@gmail.com>
2024-07-09 23:33:49 +0500

    changes commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import tasksRouter from './routes/taskRoutes.js';
+import cors from 'cors'; 
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/api', tasksRouter);
+app.use(cors());
 
 connectDB();
 
