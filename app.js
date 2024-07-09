7ebbe704e40796039e1f9b3b997ea3218eb91bf5
Abdulbosit <acunnatov@gmail.com>
2024-07-10 04:43:52 +0500

    changes commit

--- app.js
+++ app.js
@@ -4,18 +4,12 @@ import connectDB from './config/db.js';
 import tasksRouter from './routes/taskRoutes.js';
 import cors from 'cors'; 
 import helmet from 'helmet';
-import createProxyMiddleware from 'http-proxy-middleware'
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use('/api', tasksRouter);
-app.use('/api', createProxyMiddleware({ 
-  target: 'https://backend-task-manager-d780107752ab.herokuapp.com',
-  changeOrigin: true,
-  secure: false,
-}));
 app.use(cors());
 app.use(helmet())
 connectDB();
