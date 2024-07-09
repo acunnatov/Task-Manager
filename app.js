b2c00ad81b68e918d35e6aa286380c8dd89c097a
Abdulbosit <acunnatov@gmail.com>
2024-07-09 23:59:14 +0500

    changes commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import tasksRouter from './routes/taskRoutes.js';
 import cors from 'cors'; 
+import helmet from 'helmet';
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use('/api', tasksRouter);
 app.use(cors());
-
+app.use(helmet())
 connectDB();
 
 export default app;
